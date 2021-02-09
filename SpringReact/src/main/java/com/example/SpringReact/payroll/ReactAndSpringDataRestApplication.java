147128958409df8e55d0ada86921a35e89191fb5
neichmann <niklas.eichmann@web.de>
2021-02-09 21:22:55 +0100

    build rest API

---
+++ SpringReact/src/main/java/com/example/SpringReact/payroll/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,9 @@
+package com.example.SpringReact.payroll;
+
+import org.springframework.boot.SpringApplication;
+
+public class ReactAndSpringDataRestApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+	}
+}
